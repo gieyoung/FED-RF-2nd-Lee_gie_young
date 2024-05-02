70f4dd5ddd9b273c28fef235a2311d0bf361266c
gieyoung <poohlee23@naver.com>
2024-05-02 18:19:32 +0900

    f

---
+++ 003.JS학습/01.JS실험실/js/auto_scroll_main.js
@@ -0,0 +1,11 @@
+// 자동스크롤 호출하는 메인 JS - auto_scroll_main.js
+
+// 자동스크롤 함수 불러오기
+import AutoScrollFn from './auto_scroll.js';
+
+// 함수호출
+// ->생성자 함수 이므로 new키워드로 호출한다.
+const scrollObj = new AutoScrollFn();
+
+
+console.log(scrollObj);
