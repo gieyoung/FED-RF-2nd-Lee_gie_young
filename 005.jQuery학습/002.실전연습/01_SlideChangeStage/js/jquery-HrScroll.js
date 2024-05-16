eaed10d7e7610f70f5e13f1116c3cd985dec09ad
gieyoung <poohlee23@naver.com>
2024-05-16 18:16:54 +0900

    d

---
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
@@ -0,0 +1,100 @@
+// 가로 방향 스크롤 구현 JS
+
+// 1. 대상요소 :  html,body
+const scTarget = $("html,body");
+
+// 2. 스크롤 이벤트 설정 및 기능구현하기
+// 제이쿼리에서 전체 스크롤/휠 이벤트 작성시
+// 대상은 항상 html,body 로 두개 모두 잡아준다!
+// 참고로 document나 window는 사용안됨!
+// 이벤트 메서드: on(이벤트명,함수)
+// 유의사항: 휠 이벤트는 모바일과 무관하다!
+// 따라서 모바일 터치 가로 스크롤은 그냥
+// 가로 스크롤바만 살려주면 자연스럽게 된다!
+
+// 스크롤 위치값변수
+let scPos = 0;
+
+// 페이지개수
+let pgCnt = $(".page").length;
+
+// winW : 윈도우 가로크기
+// maxLimit : 최대한계값
+let winW, maxLimit;
+
+// 한번에 움직일 스크롤 이동크기
+// 상수는 모두 대문자로 쓰고 중간에 언더바로 구분함
+const MOVE_NUM = 100;
+
+// 한계값 계산함수 /////
+const chgLimit = () => {
+  winW = $(window).width();
+  // 최대한계값 : (전체이동박스크기) - 화면가로크기
+  maxLimit = winW * pgCnt - winW;
+
+  console.log(
+    "window크기:",
+    winW,
+    "/페이지수:",
+    pgCnt,
+    "/최대한계값:",
+    maxLimit
+  );
+}; /////// chgLimit 함수 ////////
+
+// 최초 한계값 계산함수 호출
+chgLimit();
+
+// 윈도우 사이즈 변경시 한계값 업데이트
+$(window).resize(chgLimit);
+// resize() 메서드 :  리사이즈 이벤트함수
+
+/////////////////////////////////////
+// 스크롤 이벤트 함수 구현하기 /////////
+/////////////////////////////////////
+scTarget.on("wheel", (e) => {
+  // 스크롤이동을 위한 제이쿼리 속성
+  // 1. scrollTop : 세로스크롤바위치
+  // 2. scrollLeft : 가로스크롤바위치
+
+  // 휠방향 알아내기(전체이벤트객체로 부터 얻어옴)
+  // (1) evnet.wheelDelta 값 :
+  // 기본이동 100px + 앞뒤예비공백 10px*2=20px
+  // 전체 표시수치 120 -> 120px을 의미
+  // 방향은 마이너스가 아랫방향
+  //   let delta = event.wheelDelta;
+  // (2) event.deltaY
+  // 기본이동크기만 표시함 즉, 100px이동값표시
+  // 방향은 양수가 아랫방향
+  // -> wheelDelta보다 나중에 나온 실질적인 표시객체임
+  //
+  let delta = event.deltaY;
+
+  // 방향에 따를 증감은 deltaY는 양수가 아랫방향
+  // wheelDelta는 음수가 아랫방향임!
+  if (delta > 0) scPos += MOVE_NUM;
+  else scPos -= MOVE_NUM;
+
+  // 한계값 체크
+  // (1) 최소한계 : 0
+  if (scPos <= 0) scPos = 0;
+  // (2) 최대한계 : 전체이동박스크기-화면가로크기
+  if (scPos >= maxLimit) scPos = maxLimit;
+
+  // scPos = scPost + 200;
+  console.log("스위:", scPos, delta);
+
+  // animate({CSS설정},시간,이징,함수)
+  // stop() 메서드 : 큐에 쌓인 애니메이션을 지운다!
+  // 중간에 쌓인 애니를 지우고 최종애니만 실행한다!
+  scTarget.stop().animate(
+    // css설정
+    {
+      scrollLeft: scPos + "px",
+    },
+    // 시간(1/1000초)
+    2000,
+    // 이징(가속도:처음에 빠르게 나중에 천천히)
+    "easeOutQuart"
+  );
+}); //////////// wheel 이벤트 구역 //////////
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/sub_cont.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/sub_cont.js
@@ -3,90 +3,164 @@
 // 데이터 셋팅 불러오기 //////
 import * as dkbData from "../data/dkb_data.js";
 
-export default function showSubBox(){
-    // console.log("나를 부르면 서브가 보여~!");
+// console.log(dkbData);
 
-    // 1. 서브 컨텐츠 보이기 기능 구현 ///
-    // (1) 대상 선정 
-    // (1-1)이벤트 대상 : 
-    // 미리보기 : .preview-box li
-    // 현장포토 : .live-box li
-    // 대표포스터 : .poster-box li
-    // 최신동영상 : .clip-box li
-    const subViewBox = 
-    $(`
+export default function showSubBox() {
+  // console.log("나를 부르면 서브가 보여~!");
+
+  // 1. 서브 컨텐츠 보이기 기능 구현 ///
+  // (1) 대상 선정
+  // (1-1)이벤트 대상 :
+  // 미리보기 : .preview-box li
+  // 현장포토 : .live-box li
+  // 대표포스터 : .poster-box li
+  // 최신동영상 : .clip-box li
+  const subViewBox = $(`
         .preview-box li,
         .live-box li,
         .poster-box li,
         .clip-box li
     `);
 
-    // (1-2) 변경대상 : .sub-cont
-    const subContBox = $(".sub-cont");
+  // (1-2) 변경대상 : .sub-cont
+  const subContBox = $(".sub-cont");
 
-    // console.log(subViewBox);
+  // console.log(subViewBox);
 
-    // 2. 이벤트 설정 및 함수구현하기 ////
-    subViewBox.click(function(){
-        let confPrt = 
-        $(this).parent().parent().is(".preview-box");
-        // parent() 바로위 상위요소로 이동
-        // 두번 위로 이동해서 li위 ul위 div
-        // 그 div박스의 클래스가 preview-box인가?
-        // is(클래스명) 메서드로 알아봄
+  // 2. 이벤트 설정 및 함수구현하기 ////
+  subViewBox.click(function () {
+    // let confPrt =
+    // $(this).parent().parent().is(".preview-box");
+    // parent() 바로위 상위요소로 이동
+    // 두번 위로 이동해서 li위 ul위 div
+    // 그 div박스의 클래스가 preview-box인가?
+    // is(클래스명) 메서드로 알아봄
 
-        // JS문법에서는 아래와 같음!
-        // this.parentElement.parentElement
-        // .classList.contains(클래스명)
+    // [ 데이터명을 data-db에 넣고 읽어오기 ]
+    // 사용하고자 하는 데이터 이름을 ul태그의
+    // data-db 속성에 담아 놓고 이것을 읽어온다!
+    let db = $(this).parent().attr("data-db");
+    // $(this).parent()는 li 바로위의 부모인 ul이다!
+    // attr('data-db') 속성값 읽어오기!
 
-        console.log("나야나!",this,confPrt);
+    // JS문법에서는 아래와 같음!
+    // this.parentElement.parentElement
+    // .classList.contains(클래스명)
 
-        if(confPrt){
-            // 1. 키속성값 읽어오기
-            let idx = $(this).attr("data-idx");
-            // attr(속성명) -> 속성값 읽어오기 메서드
-            // attr(속성명,속성값) -> 속성값 넣기 메서드
-            console.log("idx:",idx);
+    console.log("나야나!", this, db, dkbData[db]);
 
-            // [ 배열순회 메서드 비교 : forEach / find ]
-            // forEach() 는 모두 순회한다!
-            // find() 는 조건에 맞을때 return true하면
-            // 해당 배열값이 변수에 할당된다!
-            // 만약 일치하는 데이터가 없으면 undefined됨!
+    // if (confPrt) {
+    // 1. 키속성값 읽어오기
+    let idx = $(this).attr("data-idx");
+    // attr(속성명) -> 속성값 읽어오기 메서드
+    // attr(속성명,속성값) -> 속성값 넣기 메서드
+    console.log("idx:", idx);
 
-            // dkbData.previewData.forEach(v=>{
-            let selData = dkbData.previewData.find(v=>{
-                if(v.idx == idx){
-                    // console.log("찾았다!",v);
-                    return true;
-                }
-                console.log("돌아!");
+    // [ 배열순회 메서드 비교 : forEach / find ]
+    // forEach() 는 모두 순회한다!
+    // find() 는 조건에 맞을때 return true하면
+    // 해당 배열값이 변수에 할당된다!
+    // 만약 일치하는 데이터가 없으면 undefined됨!
 
-            });
+    // dkbData.previewData.forEach(v=>{
 
-            console.log("검색결과:",selData);
-
-            // 서브박스에 내용 넣기
-            // 제이쿼리는 innerHTML 할당대신
-            // html() 메서드를 사용한다!
-            subContBox.html(`
-                <button class="cbtn">×</button>
-                <div class="sub-inbox inbox">
-                    <h1>${selData.title}</h1>
-                    <div class="sub-item">
-                        ${selData.story}
-                    </div>
-                </div>
-            `).show();
-            // show() 는 display를 보여주는 메서드
-            // hide() 는 display를 숨기는 메서드
-            // toggle() 는 display를 토글하는 메서드
-
-            // 닫기버튼 이벤트 설정하기
-            $(".cbtn").click(()=>subContBox.hide());
-
-        } /// if /////
-
+    // dkbData[db] -> 해당데이터 매칭하기!
+    let selData = dkbData[db].find((v) => {
+      if (v.idx == idx) {
+        // console.log("찾았다!",v);
+        return true;
+      }
+      console.log("돌아!");
     });
 
+    console.log("검색결과:", selData);
+
+    // 이미지의 개수를 반영한 배열을 임의로 만들고
+    // 필요한 경우 이 배열로 map()을 돌려서 코드를 생성한다!
+    // 우선 빈배열을 만든다!
+    let iarr = [];
+    // 현장포토일때 사용
+    if (db == "liveData") {
+      for (let i = 0; i < selData.imgName[1]; i++) iarr[i] = "";
+
+      console.log("이미지map을 위한 배열:", iarr);
+    } /// if ///
+
+    // 서브박스에 내용 넣기
+    // 제이쿼리는 innerHTML 할당대신
+    // html() 메서드를 사용한다!
+    subContBox
+      .html(
+        // 1.미리보기 출력
+        db == "previewData"
+          ? `
+            <button class="cbtn">×</button>
+            <div class="sub-inbox inbox">
+                <h1>${selData.title}</h1>
+                <div class="sub-item">
+                    ${selData.story}
+                </div>
+            </div>
+          `
+          : // 2.현장포토 출력
+          db == "liveData"
+          ? `
+          <button class="cbtn">×</button>
+            <div class="sub-inbox inbox">
+                <h1>현장포토 : ${selData.title}</h1>
+                <div class="sub-item">
+                ${iarr
+                  .map(
+                    (v, i) => `
+                <img 
+                  src="./images/live_photo/${selData.imgName[0]}/${i + 1}.jpg" 
+                  alt="${selData.title}" />
+                `
+                  )
+                  .join("")}
+                </div>
+            </div>
+          `
+          : // 3.대표 포스터 출력
+          db == "posterData"
+          ? `
+          <button class="cbtn">×</button>
+            <div class="sub-inbox inbox">
+                <h1>대표 포스터 : ${selData.title}</h1>
+                <div class="sub-item">
+                    <img 
+                      src="./images/poster_img/${selData.imgName}.jpg" 
+                      alt="${selData.title}" />
+                </div>
+            </div>
+          `
+          : // 4.최신 동영상 출력
+          db == "clipData"
+          ? `
+          <button class="cbtn">×</button>
+            <div class="sub-inbox inbox">
+                <h1>클립영상 : ${selData.title}</h1>
+                <div class="sub-item">
+                  <iframe src="https://www.youtube.com/embed/${selData.mvid}?autoplay=1" allow="autoplay"></iframe>
+                  <h2>${selData.subtit}</h2>
+                </div>
+            </div>
+          `
+          : // 5.위의 해당사항이 없을 경우
+            `
+          <button class="cbtn">×</button>
+          <div class="sub-inbox inbox">
+              <h1>DB정보확인필요!</h1>
+          </div>
+          `
+      )
+      .show();
+    // show() 는 display를 보여주는 메서드
+    // hide() 는 display를 숨기는 메서드
+    // toggle() 는 display를 토글하는 메서드
+
+    // 닫기버튼 이벤트 설정하기
+    $(".cbtn").click(() => subContBox.hide());
+    // } /// if /////
+  });
 } /////////// showSubBox 함수 ///////////////
