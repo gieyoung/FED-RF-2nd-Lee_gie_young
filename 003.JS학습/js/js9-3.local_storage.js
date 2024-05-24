b88343a24007df9897dfb1f9bd02e9adf928a124
gieyoung <poohlee23@naver.com>
2024-05-24 09:50:37 +0900

    s

--- 003.JS학습/js/js9-3.local_storage.js
+++ 003.JS학습/js/js9-3.local_storage.js
@@ -212,8 +212,9 @@ function bindData(){
 
 } ////////////// bindData //////////////////
 
-// 게시판 최초호출
-bindData();
+// 게시판 최초호출 : 로컬쓰 minfo 존재여부에 따라 처리
+if(localStorage.getItem("minfo")) bindData();
+else makeObj();
 
 
 ///// 게시판 입력 버튼 클릭시 구현하기 //////
@@ -228,19 +229,58 @@ mFn.qs("#sbtn").onclick=()=>{
     
     console.log("입력!!!",localData);
 
-    // 2. 입력할 데이터 객체형식으로 배열에 넣기
+    // 2. 입력값이 비었으면 돌려보내기!
+    // trim() - 앞뒤공백 제거 메서드
+    if(mFn.qs("#tit").value.trim() == ""
+     || mFn.qs("#cont").value.trim() == ""){
+        alert("제목과 내용입력은 필수입니다!");
+        return;
+     } ////// if //////
+
+    console.log("idx값배열:",localData.map(v=>v.idx));
+
+    // 3. 입력할 데이터 객체형식으로 배열에 넣기
     // 배열.push({객체})
     localData.push({
-        idx: localData.length+1,
+        // 순번은 배열객체 idx값중 최대값을 구하여 1더한다!
+        // apply(보낼객체,배열) -> 보낼객체가 여기서는 null
+        idx: Math.max.apply(null,localData.map(v=>v.idx))+1,
         tit: mFn.qs("#tit").value,
         cont: mFn.qs("#cont").value
     });
 
-    // 3. 배열 데이터를 문자화하여 로컬쓰에 입력
+    // 4. 배열 데이터를 문자화하여 로컬쓰에 입력
     localStorage
     .setItem("minfo", JSON.stringify(localData));
 
-    // 4. 화면출력 함수 호출하기
+    // 5. 화면출력 함수 호출하기
     bindData();
 
 }; /////// click 함수 ///////////
+
+// CRUD 크루드!!!
+// Create(만들기) / Read(읽기) / Update(수정) / Delete(삭제)
+
+///////// 수정기능 구현하기 //////////////
+
+// 수정항목 선택박스 업데이트함수 호출
+updateItemList();
+
+// 수정할 항목 업데이트 함수 /////
+function updateItemList(){
+    // 대상: 수정선택박스 - #sel
+    const selBox = mFn.qs("#sel");
+
+    // 데이터의 idx를 순회하며 option만들기
+    const localData = 
+    JSON.parse(localStorage.getItem("minfo"));
+
+    selBox.innerHTML = 
+    localData.map(
+        v=>`
+            <option value="${v.idx}">
+                ${v.idx}</option>
+        `).join('');
+
+
+} //////////// updateItemList 함수 ///////////
--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -1,2 +1,28 @@
 // 보그 PJ 메인 JS - main.js
 
+// 메인페이지 전체 레이아웃 로딩 컴포넌트
+
+// 상단영역 불러오기
+import TopArea from "./components/TopArea";
+// 메인영역 불러오기
+import MainArea from "./components/MainArea";
+// 하단영역 불러오기
+import FooterArea from "./components/FooterArea";
+
+function Layout() {
+  return (
+    <React.Fragment>
+      {/* //  상단영역 컴포넌트 */}
+      <TopArea />
+
+      {/* // 메인영역 */}
+      <MainArea />
+
+      {/* // 하단영역 컴포넌트 */}
+      <FooterArea />
+    </React.Fragment>
+  );
+} //////////Layout컴포넌트//////////
+
+// 상단영역만 넣어보기
+ReactDOM.render(<Layout/>, document.querySelector("#root"));
