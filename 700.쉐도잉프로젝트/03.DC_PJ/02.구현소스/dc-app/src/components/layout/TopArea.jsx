9d308ffb45e03e810cf95f41a6edca671df568a6
gieyoung <poohlee23@naver.com>
2024-06-04 18:17:00 +0900

    d

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -2,7 +2,11 @@
 
 // GNB 데이터 불러오기
 import { Link } from "react-router-dom";
-import {menu} from "../data/gnb";
+import { menu } from "../data/gnb";
+
+// 상단영역 CSS 불러오기
+import "../../css/top_area.scss";
+import Logo from "../modules/Logo";
 
 export default function TopArea() {
   //// 코드 리턴구역 //////////////
@@ -11,19 +15,42 @@ export default function TopArea() {
       {/* 1.상단영역 */}
       <header className="top-area">
         {/* 로그인 환영메시지 박스 */}
-        
+
         {/* 네비게이션 GNB파트 */}
         <nav className="gnb">
           <ul>
             {/* 1. 로고 컴포넌트 */}
             <li>
-              
+              <Logo logoStyle="top" />
             </li>
             {/* 2. GNB메뉴 데이터 배열로 만들기 */}
-            {menu.map((v,i)=>
-            <li key={i}>
-                <Link to={v.link}>{v.txt}</Link>
-            </li>)}
+            {menu.map((v, i) => (
+              <li key={i}>
+                {
+                  // 하위 메뉴가 있으면 일반 a요소에 출력
+                  // 없으면 Link 라우팅 출력
+                  v.sub ? (
+                    <a href="#">{v.txt}</a>
+                  ) : (
+                    <Link to={v.link}>{v.txt}</Link>
+                  )
+                }
+                {
+                  // 서브 메뉴 데이터가 있으면 하위그리기
+                  v.sub && (
+                    <div className="smenu">
+                      <ol>
+                        {v.sub.map((v, i) => (
+                          <li key={i}>
+                            <Link to={v.link}>{v.txt}</Link>
+                          </li>
+                        ))}
+                      </ol>
+                    </div>
+                  )
+                }
+              </li>
+            ))}
           </ul>
         </nav>
       </header>
