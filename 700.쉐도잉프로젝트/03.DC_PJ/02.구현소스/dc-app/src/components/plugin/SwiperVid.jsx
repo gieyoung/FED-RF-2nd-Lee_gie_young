017541f0d395dd03f4d0757e808b8e1c47995265
gieyoung <poohlee23@naver.com>
2024-06-12 17:31:10 +0900

    f

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
@@ -4,9 +4,16 @@ import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// 제이쿼리 불러오기
+import $ from "jquery";
+
 // Import Swiper styles : 모듈용 기본 CSS파일 로딩!
 import "swiper/css";
 import "swiper/css/navigation";
+
+// 폰트어썸 불러오기
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
 
 // 스와이퍼 비디오 모듈 CSS : 내가 작성한 CSS
 import "./css/swiper_vid.scss";
@@ -25,23 +32,78 @@ export function SwiperVid({ catName }) {
   // 선택데이터 변수할당
   const selData = swVidData[catName];
 
+  // 비디오 팝업 함수
+  const showVideo = (src, tit) => {
+    console.log("비디오보여", src, tit);
+    // 1. 대상선정
+    // 1-1. 아이프레임 : .play-vid iframe
+    const ifr = $(".play-vid iframe");
+    // 1-2. 전체 박스 : .vid-bx
+    const vbx = $(".vid-bx");
+    // 1-3. 타이틀 박스 : .ifr-tit
+    const itit = $(".ifr-tit");
+    // 1-4. 닫기 버튼 : .cbtn
+    const cbtn = $(".cbtn");
+
+    // 2. 변경하기
+    // 2-1. 아이프레임 src경로 넣기
+    ifr.attr("src", src + "?autoplay=1");
+
+    // 2-2. 타이틀 넣기
+    itit.text(tit);
+
+    // 2-3.박스보이기
+    vbx.fadeIn(300);
+
+    // 2-4. 닫기버튼 클릭시
+    cbtn.on("click", () => {
+      vbx.fadeOut(300);
+      ifr.attr("src", "");
+    });
+  }; /////////showVideo/////////
+
   return (
     <>
       <Swiper
-        slidesPerView={4}
+        // slidesPerView={4}
         spaceBetween={20}
         navigation={true}
         /* 사용할 모듈을 여기에 적용시킨다 */
         modules={[Navigation]}
+        // 스와이퍼 사이즈별 슬라이드수 변경!
+        breakpoints={{
+          200: {
+              slidesPerView: 1,
+          },
+          500: {
+              slidesPerView: 2,
+          },
+          1000: {
+              slidesPerView: 3,
+          },
+          1200: {
+              slidesPerView: 4,
+          },
+        }}
         className="mySwiper"
       >
         {selData.map((v, i) => (
           <SwiperSlide key={i}>
-            <section className="sw-inbox">
+            <section className="sw-inbox" onClick={() => showVideo(v.vsrc, v.tit)}>
               {/* 동영상이미지박스 */}
               <div className="vid-img">
                 <img src={v.isrc} alt={v.tit} />
                 {/* 폰트어썸 아이콘 */}
+                <FontAwesomeIcon
+                  icon={faCirclePlay}
+                  style={{
+                    position: "absolute",
+                    bottom: "55%",
+                    left: "10%",
+                    color: "#fff",
+                    fontSize: "50px",
+                  }}
+                />
               </div>
               {/* 동영상 타이틀 박스 */}
               <div className="vid-tit">
