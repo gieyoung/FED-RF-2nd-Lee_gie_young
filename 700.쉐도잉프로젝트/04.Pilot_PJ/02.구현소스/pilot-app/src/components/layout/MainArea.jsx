9c1015c6e1ec60e284d25c38cb314f3908c626e3
gieyoung <poohlee23@naver.com>
2024-06-20 09:34:53 +0900

    r

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import MainCont from '../pages/MainCont';
+import React from "react";
+import MainCont from "../pages/MainCont";
+import Fashion from "../pages/Fashion";
 
-function MainArea(props) {
-    return (
-        <>
-            <MainCont />
-        </>
-    );
+function MainArea({ page, xxx }) {
+  // page 페이지변경 문자값전달
+  // 5가지 값 : main / glist / men / women / style
+  console.log("메인전달page:",page);
+
+  // 코드 리턴구역 /////////////
+  return (
+    <>
+      {/* 조건출력으로 페이지별 분기 */}
+      {page == "main" && <MainCont />}
+      {page == "fashion" && <Fashion />}
+    </>
+  );
 }
 
 export default MainArea;
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import TopArea from './components/layout/TopArea';
 import MainArea from './components/layout/MainArea';
 import FooterArea from './components/layout/FooterArea';
 
+// 컨텍스트 API 불러오기
+import { pCon } from './components/modules/pCon';
+
 // 전체 공통 CSS
 import "./css/index.scss";
 
 function MainComponent(props) {
+  // 상태관리 변수 셋팅 ///////
+  // 1. 페이지변경 상태변수
+  const [pgName, setPgName] = useState("main");
+
+  /**************************************** 
+    [ 컨텍스트 API 공개 변수들 ]
+    1. setPgName - 페이지이름 업데이트메서드
+  ****************************************/
+
+  // 코드 리턴구역 /////////////
   return (
-    <>
+    <pCon.Provider value={{setPgName}}>
       <TopArea />
-      <MainArea />
+      <MainArea page={pgName} />
       <FooterArea />
-    </>
+    </pCon.Provider>
   );
 }
 
